6f10e7e66dfb55522175517a27b9df12f9d97487
divyendu <divyendua@gmail.com>
2024-04-20 01:49:45 -0400

    get in reversed manner

--- Posts/dao.js
+++ Posts/dao.js
@@ -3,13 +3,13 @@ export const createPost = (post) => {
     delete post._id
     return model.create(post);
 }
-export const findAllPosts = () => model.find();
-export const findPostByUser = (userid) => model.find({ userid: userid });
+export const findAllPosts = () => model.find().sort({date: -1});
+export const findPostByUser = (userid) => model.find({ userid: userid }).sort({date: -1});
 export const updatePost = (postId, post) => model.updateOne({ _id: postId }, { $set: post });
 export const deletePost = (postId) => model.deleteOne({ _id: postId });
 export const findPostOfFollowing = async (userids) => {
     const query = { userid: { $in: userids } };
-    return await model.find(query)
+    return await model.find(query).sort({date: -1})
         .then(docs => {
             return docs
         })
