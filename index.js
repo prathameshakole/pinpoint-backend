0cbc2cd79def92fcd1f63d6b40dbdc633aa18b8c
Prathamesh Akole <akoleprathamesh@gmail.com>
2025-02-26 22:09:02 -0500

    changed indexjs

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors({
     credentials: true,
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL,
 }));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
